2e272c17f9986c1ccc23c98253b0b2fb4202a9ce
apichaya <apichaya@apichayas-MacBook-Air.local>
2017-08-30 15:09:18 +0700

    Initial Commit

---
+++ NileconHR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NileconHR
+//
+//  Created by apichaya on 8/30/2560 BE.
+//  Copyright © 2560 apichaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
